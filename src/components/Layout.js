690413e2bedc78e64728677a90392df1f9d1c259
Mateusz <mateusz-wojcik@hotmail.com>
2021-08-06 23:58:12 +0200

    added sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 
 import "../assets/css/main.css"
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    console.log("dupa")
+    setIsOpen(!isOpen)
+  }
   return (
     <>
-      <Navbar />
+      <Navbar toggleSidebar={toggleSidebar} />
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       {children}
       <Footer />
     </>
